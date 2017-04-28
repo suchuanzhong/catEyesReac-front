e3700e6a3d3b5c9b6c2c33ced870e72ffd858a9f
SCZ <1219498646@qq.com>
2017-04-28 20:12:01 +0800

    4.28晚

--- antd/cinemaMate/MateTable.js
+++ antd/cinemaMate/MateTable.js
@@ -9,52 +9,52 @@ export default class MateTable extends React.Component{
     }
     render(){
     	const columns = [{
-				  title: '电影名',
-				  dataIndex: 'movieCname',
-				  key: 'movieCname',
-				}, {
-				  title: '英文名',
-				  dataIndex: 'movieEname',
-				  key: 'movieEname',
-				}, {
-				  title: '电影类型',
-				  dataIndex: 'movieType',
-				  key: 'movieType',
-				}, {
-				  title: '区域',
-				  dataIndex: 'movieAddress',
-				  key: 'movieAddress',
-				}, {
-				  title: '上映时间',
-				  dataIndex: 'movieAge',
-				  key: 'movieAge',
-				}, {
-				  title: '时长',
-				  dataIndex: 'movieTime',
-				  key: 'movieTime',
-				}, {
-				  title: '首映时间',
-				  dataIndex: 'movieShowTime',
-				  key: 'movieShowTime',
-				}, {
-				  title: '首映区域',
-				  dataIndex: 'movieShowAdd',
-				  key: 'movieShowAdd',
-				}, {
-				  title: '票房',
-				  dataIndex: 'movieMoney',
-				  key: 'movieMoney',
-				}, {
-				  title: '操作',
-				  dataIndex: 'action',
-				  key: 'action',
-				  render: (text, record) => (
-				    <span>
-				      
-				      <Button>增加院线</Button>	
-				      <Button>删除</Button>	
-				    </span>
-				  ),
+					  title: '电影名',
+					  dataIndex: 'movieCname',
+					  key: 'movieCname',
+						}, {
+					  title: '英文名',
+					  dataIndex: 'movieEname',
+					  key: 'movieEname',
+						}, {
+					  title: '电影类型',
+					  dataIndex: 'movieType',
+					  key: 'movieType',
+						}, {
+					  title: '区域',
+					  dataIndex: 'movieAddress',
+					  key: 'movieAddress',
+						}, {
+					  title: '上映时间',
+					  dataIndex: 'movieAge',
+					  key: 'movieAge',
+						}, {
+					  title: '时长',
+					  dataIndex: 'movieTime',
+					  key: 'movieTime',
+						}, {
+					  title: '首映时间',
+					  dataIndex: 'movieShowTime',
+					  key: 'movieShowTime',
+						}, {
+					  title: '首映区域',
+					  dataIndex: 'movieShowAdd',
+					  key: 'movieShowAdd',
+						}, {
+					  title: '票房',
+					  dataIndex: 'movieMoney',
+					  key: 'movieMoney',
+						}, {
+					  title: '操作',
+					  dataIndex: 'action',
+					  key: 'action',
+					  render: (text, record) => (
+					    <span>
+					      
+					      <Button>增加院线</Button>	
+					      <Button>删除</Button>	
+					    </span>
+					  ),
 				}];
 		return <div >
 			<Card>
